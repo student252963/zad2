2fd21f7714207f87dd69c5b270e9fae06da87729
Kacper Raczyński <252963@student.pwr.edu.pl>
2020-03-15 20:26:17 +0100

    Add initial files

---
+++ src/LZespolona.cpp
@@ -0,0 +1,20 @@
+#include "LZespolona.hh"
+
+
+
+/*!
+ * Realizuje dodanie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik dodawania,
+ *    Skl2 - drugi skladnik dodawania.
+ * Zwraca:
+ *    Sume dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Skl1.re + Skl2.re;
+  Wynik.im = Skl1.im + Skl2.im;
+  return Wynik;
+}
